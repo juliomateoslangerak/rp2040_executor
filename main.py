7a55510c0d58d034f10276367fbc43d4a0e3a011
Julio Mateos Langerak <julio.matoes-langerak@igh.cnrs.fr>
2022-02-27 23:39:47 +0100

    Added some initial state machine

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import utime
 from machine import Pin
+from rp2 import PIO, StateMachine, asm_pio
+
+
+@asm_pio(set_init=(PIO.OUT_LOW,) * 4,
+         out_init=(PIO.OUT_LOW,) * 4,
+         out_shiftdir=PIO.SHIFT_RIGHT,
+         in_shiftdir=PIO.SHIFT_LEFT)
+def run_digitals():
+    pull()
+    mov(x, osr)  # num steps
+
+    pull()
+    mov(y, osr)  # step pattern
+
+    jmp(not_x, "end")
+
+    label("loop")
+    jmp(not_osre, "step")  # loop pattern if exhausted
+    mov(osr, y)
+
+    label("step")
+    out(pins, 4)[31]
+
+    jmp(x_dec, "loop")
+    label("end")
+
+    irq(rel(0))
+
+
+sm = StateMachine(1, prog=run_digitals, freq=100000)
 
 
 def main():
