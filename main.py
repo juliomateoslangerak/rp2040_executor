856b1ad528f2dd7c600cd5e85b478fe7159ed1b4
julio <juiio.mateos-langerak@igh.cnrs.fr>
2022-03-03 03:16:35 +0100

    Example working

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-import time
-import rp2
 from machine import Pin
+from rp2 import PIO, StateMachine, asm_pio
+from time import sleep
 
-# Define the blink program.  It has one GPIO to bind to on the set instruction, which is an output pin.
-# Use lots of delays to make the blinking visible by eye.
-@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
-def blink():
+
+@asm_pio(
+         # set_init=PIO.OUT_LOW,
+         out_init=(PIO.OUT_LOW,) * 32,
+         out_shiftdir=PIO.SHIFT_LEFT,
+         fifo_join=PIO.JOIN_TX,
+)
+def prog():
     wrap_target()
-    set(pins, 1)   [31]
-    nop()          [31]
-    nop()          [31]
-    nop()          [31]
-    nop()          [31]
-    set(pins, 0)   [31]
-    nop()          [31]
-    nop()          [31]
-    nop()          [31]
-    nop()          [31]
+    pull()
+    mov(x, osr)
+    jmp(not_x, 'end')
+    pull()
+    out(pins, 32)
+    label('loop')
+    jmp(x_dec, 'loop')
     wrap()
+    label('end')
+    mov(pins, null)
 
-# Instantiate a state machine with the blink program, at 2000Hz, with set bound to Pin(25) (LED on the rp2 board)
-sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(25))
 
-# Run the state machine for 3 seconds.  The LED should blink.
+sm = StateMachine(1, prog, freq=2000, out_base=Pin(0))
+
 sm.active(1)
-time.sleep(3)
+for _ in range(3):
+    sm.put(1000)
+    sm.put(0b00000010000000000000000000000001)  # 1000 0100 0010 0001 1000010000100001
+    sm.put(1000)
+    sm.put(0b00000000000000000000000000000000)  # 1000 0100 0010 0001 1000010000100001
+    sm.put(1000)
+    sm.put(0b00000010000000000000000000000001)  # 1000 0100 0010 0001 1000010000100001
+    sm.put(1000)
+    sm.put(0b00000000000000000000000000000000)  # 1000 0100 0010 0001 1000010000100001
+    sm.put(0)
+    sm.put(0b00000000000000000000000000000000)  # 1000 0100 0010 0001 1000010000100001
+sleep(20)
 sm.active(0)
